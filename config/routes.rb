0a9304456906bff0d0dbd3b4c642163d90ddf487
danilomrls <danilopalhetam@gmail.com>
2020-06-04 14:52:08 -0300

    creating CRUDS

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :reservations
+  resources :librarians
+  resources :clients
+  resources :books
+  resources :categories
+  resources :authors
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
